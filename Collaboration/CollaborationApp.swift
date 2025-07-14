3fdfc57af55b39598f3333ff9c1bafd7a00e6b94
Harini Kondisetty <harini.kondisetty@gmail.com>
2025-07-14 09:38:09 -0500

    Initial Commit

---
+++ Collaboration/CollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollaborationApp.swift
+//  Collaboration
+//
+//  Created by Scholar on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
